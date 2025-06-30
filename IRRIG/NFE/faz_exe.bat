a95c76a406b278c5b6a7babf0aa36719f5f79b4b
NicolasIrrigPenapolis <nicolasportie@irrigpenapolis.com.br>
2025-06-30 15:19:06 -0300

    Adicionando picturebox no frm Conpedi

--- IRRIG/NFE/faz_exe.bat
+++
@@ -1,1 +0,0 @@
-"c:\program files (x86)\microsoft visual studio\vb98\vb6.exe" "c:\prg\emissor de nfe\nfe\nfe.vbp" /make "C:\PRG\EMISSOR DE NFE\NFE\NFE.exe"
--- IRRIG/NFE/faz_res.bat
+++
@@ -1,3 +0,0 @@
-cd C:\PRG\EMISSO~2\figuras\
-"c:\program files (x86)\microsoft visual studio\vb98\wizards\rc.exe" -r -fo "c:\prg\emissor de nfe\nfe\nfe".res "c:\prg\emissor de nfe\nfe\nfe".rc
-cd C:\PRG\EMISSOR DE NFE\NFE\
